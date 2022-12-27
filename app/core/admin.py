cf8563eaed468e9c6f770049e4cc2c365a0df42b
mdjokovic <mdjokovic@mds.rs>
2022-12-27 11:16:56 +0100

    Update import model from core app

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,11 +1,8 @@
-# from django.contrib import
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
 
-
-# Register your models here.
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
