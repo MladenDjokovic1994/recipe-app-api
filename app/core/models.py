ec64e6db7e956c12c2c106e3bac2aef6e985f23b
mdjokovic <mdjokovic@mds.rs>
2022-12-29 11:56:10 +0100

    Create recipe model and user api endpoints

--- app/core/models.py
+++ app/core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import (
 from django.db import models
 
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -35,3 +36,18 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minute = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from decimal import Decimal
+
+from .. import models
+
 
 class ModelTests(TestCase):
 
@@ -51,3 +55,18 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Recipe name',
+            time_minute=5,
+            price=Decimal('5.50'),
+            description='Recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
